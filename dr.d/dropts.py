1de3acef08f7ae62c20c7e419fb1aa21d059114d
thutt <thutt@harp-project.com>
2025-08-30 21:24:18 -0700

    [dropts.py] Begin adding help text
    Details
    
      This, an unfinished change, begins adding descriptive help text.
      This text will prescribe how the system works, and how to use it.
      This is particularly important to do before code is written to
      understand exactly how the system will work for the various states
      of files in Git (untracked, unstaged, staged, committed).
    
    Testing
    
      Visual inspection.

--- dr.d/dropts.py
+++ dr.d/dropts.py
@@ -8,6 +8,76 @@ import os
 def configure_parser():
     description = ("""
 
+diff-review facilitates viewing the contents of changes to the
+contents of text files with a side-by-side diff utility such as
+'tkdiff' or 'meld'.
+
+There are four states in which a file can exist, and three for which
+diffs can be generated.  They are as follows:
+
+  o untracked  (All SCM)
+
+    Untracked files are unknown to the SCM.  Diffs cannot be
+    generated for untracked files.
+
+    A diagnostic will be produced for each untracked file found in
+    the source tree.
+
+  o unstaged   (Git)
+
+    An unstaged file is known to the SCM.  Diffs can be generated
+    for unstaged changes.
+
+    Diffs for unstaged changes will be made, in this order, against:
+
+    + Staged changes to the same file.
+
+    + If no changes to the same file are staged, against the most
+      recently committed change to the same file on the same branch.
+      If there is no such previous committed change, the diff will
+      show as a new file.
+
+  o staged   (Git)
+
+    Staged changes are known to the SCM.  Diffs can be generated for
+    staged changes.
+
+    Diffs for staged changes will be made against the most recently
+    committed change to the same file on the same branch.  If there is
+    no such previous committed change, the diff will show as a new
+    file.
+
+  o committed  (All SCM)
+
+    Committed changes are known to the SCM.  Diffs can be generated
+    for committed changes.
+
+    Diffs for committed changes will be made against the previous
+    committed change to the same file on the same branch.  If there is
+    no such previous committed change, the diff will show as a new
+    file.
+
+
+The tool will generate console output like the following:
+
+    <DESCRIBE THE CONSOLE OUTPUT>
+
+
+Implementation notes:
+
+  o Unstaged files take precendence over staged files; simultaneously
+    creating diffs for unstaged & staged changes to the same file is
+    not supported.
+
+  o An SCM that does not support unstaged and staged file states will
+    not support those semantics.
+
+
+""")
+
+    help_epilog = ("""
+
+
 Return Code:
   0       : success
   non-zero: failure
@@ -16,17 +86,17 @@ Return Code:
     home       = os.getenv("HOME", os.path.expanduser("~"))
     review_dir = os.path.join(home, "review")
 
-    formatter = argparse.ArgumentDefaultsHelpFormatter
+    formatter = argparse. RawDescriptionHelpFormatter
     parser    = argparse.ArgumentParser(usage           = None,
                                         formatter_class = formatter,
                                         description     = description,
+                                        epilog          = help_epilog,
                                         prog            = "diff-review")
 
     o = parser.add_argument_group("SCM Control")
     o.add_argument("-c",
-                   help     = ("Select change(s) you want to 'diff'.  "
-                               "For Git, each argument can be a value "
-                               "that can be used with 'git rev-parse'."),
+                   help     = ("Select a change, using the SCM's change "
+                               "identifier, that you want to 'diff'."),
                    action   = "append",
                    default  = [],
                    metavar  = "<change id>",
