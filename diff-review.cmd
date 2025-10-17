8a724c978ff4a7ab84c606ca1656d872e06f2b40
thutt <thutt@harp-project.com>
2025-10-16 21:48:11 -0700

    Improve windows wrappers with error handling
    Details
    
      This change updates the Windows batch files to check if python3 and
      git are available on %PATH%.  If either is not available, a fatal
      error is issued.
    
    Testing
    
      Manually verified that the error checking works by altering the name
      of the executable being searched to something that does not exist.

--- diff-review.cmd
+++ diff-review.cmd
@@ -2,11 +2,22 @@
 rem  Copyright (c) 2025  Logic Magicians Software.
 rem  All Rights Reserved.
 rem  Licensed under Gnu GPL V3.
-rem w
+rem
 rem  This wrapper script facilitates invoking the self-review Python
 rem  script.
 rem
 
-for /f "delims=" %%i in ('where git.exe') do set GP=%%i
+set PYTHON=
+for /f "delims=" %%i in ('where python3.exe 2^>nul') do set PYTHON=%%i
 
-python3 -B %~dp0\scripts.d\dr.d\dr.py --git-path %GP% %*
+if defined PYTHON (
+    set GP=
+    for /f "delims=" %%i in ('where git.exe 2^>nul') do set GP=%%i
+    if defined GP (
+        %PYTHON% -b -B -E %~dp0\scripts.d\dr.d\dr.py --git-path %GP% %*
+    ) else (
+        echo fatal: git not found; unable to generate diffs.
+    )
+) else (
+    echo fatal: python3 not found; unable to generate diffs.
+)
--- view-review.cmd
+++ view-review.cmd
@@ -2,9 +2,15 @@
 rem  Copyright (c) 2025  Logic Magicians Software.
 rem  All Rights Reserved.
 rem  Licensed under Gnu GPL V3.
-rem w
+rem
 rem  This wrapper script facilitates invoking the self-review Python
 rem  script.
 rem
 
-python3 -B %~dp0\scripts.d\vr.d\vr.py %*
+set PYTHON=
+for /f "delims=" %%i in ('where python3.exe 2^>nul') do set PYTHON=%%i
+if defined PYTHON (
+    %PYTHON%  -b -B -E  %~dp0\scripts.d\vr.d\vr.py %*
+) else (
+    echo fatal: python3 not found; unable to generate diffs.
+)
