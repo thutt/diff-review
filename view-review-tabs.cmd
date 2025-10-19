bb45f6c7e69db4b476af980cb6715a03b3057327
thutt <thutt@harp-project.com>
2025-10-19 16:17:51 -0700

    [view-review-tabs.cmd] Add batch file to access new view manager on Windows
    Details
    
      This batch file is cloned from 'view-review.cmd', with the pathname
      of the tool renamed.
    
    Testing
    
      It is at least robust as 'view-review.cmd'.
      The tabbed viewer loads properly on Windows.

---
+++ view-review-tabs.cmd
@@ -0,0 +1,16 @@
+@echo off
+rem  Copyright (c) 2025  Logic Magicians Software.
+rem  All Rights Reserved.
+rem  Licensed under Gnu GPL V3.
+rem
+rem  This wrapper script facilitates invoking the self-review Python
+rem  script.
+rem
+
+set PYTHON=
+for /f "delims=" %%i in ('where python3.exe 2^>nul') do set PYTHON=%%i
+if defined PYTHON (
+    %PYTHON%  -b -B -E  %~dp0\scripts.d\vrt.d\vrt.py %*
+) else (
+    echo fatal: python3 not found; unable to generate diffs.
+)
